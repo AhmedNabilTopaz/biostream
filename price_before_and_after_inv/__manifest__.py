ebceff4c01dfb308f70b8f81864ae1d099f8038b
Ahmed Gaber <gaber@psgsa.com.sa>
2022-07-15 20:39:42 +0300

    price before and after

---
+++ price_before_and_after_inv/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Inv Price before After",
+
+    'summary': """
+        Inv Price before After""",
+
+    'description': """
+        Inv Price before After
+    """,
+
+    'author': "Ahmed Gaber",
+    'category': 'Accounting',
+
+    'license': 'LGPL-3',
+    'currency': 'EUR',
+    'depends': ['base', 'account', ],
+
+
+    'data': [
+        'views/ks_account_invoice.xml',
+        'views/report_invoice.xml',
+
+    ],
+
+}
---
+++ price_before_and_after_inv/models/ks_account_invoice.py
@@ -0,0 +1,20 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError, ValidationError
+
+
+class AccountMoveLine(models.Model):
+
+    _inherit = "account.move.line"
+
+    default_product_price  = fields.Float(string="Price Before",related='product_id.list_price')
+
+    @api.depends('default_product_price','price_unit')
+    def calc_pricee_diff(self):
+        for rec in self:
+            if rec.product_id.list_price > rec.price_unit:
+                rec.price_diff = rec.product_id.list_price - rec.price_unit
+            else:
+                rec.price_diff = rec.price_unit - rec.product_id.list_price
+
+    price_diff = fields.Float(string="Discount",compute='calc_pricee_diff')
+
