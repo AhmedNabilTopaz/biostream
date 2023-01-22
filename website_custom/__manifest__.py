0fe5f31b746f6559e801cf602bc23f66b2892c0d
Ahmed Gaber <gaber@psgsa.com.sa>
2023-01-22 13:59:16 +0300

    biostream test

---
+++ website_custom/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    "name": "Website Address",
+    "summary": "Website Address",
+    "category": "Website",
+    "version": "15.0",
+    "sequence": 1,
+    "author": "Ahmed ezz",
+
+    "description": """Website Sale""",
+    "depends": [
+
+        'website_sale'
+    ],
+    'data': [
+        'views/website.xml',
+    ],
+
+}
