e6f6339c3188589e715e819edd96152ba5620466
Ahmed Gaber <gaber@psgsa.com.sa>
2023-09-20 19:37:12 +0300

    biostream test

---
+++ cft_hide_delete_button/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Hide Delete Option for specific users/groups',
+    'depends': [
+        'base','web'
+    ],
+    'data': [
+        'security/import_security.xml',
+    ],
+
+ }
