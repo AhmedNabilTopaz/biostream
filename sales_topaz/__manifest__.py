3b4a45bdee8a57f8b12a28dead3ad03da994dabf
Mohamed Azaz <mohaz14@outlook.com>
2025-01-02 21:22:32 +0200

    add sales edits

---
+++ sales_topaz/__manifest__.py
@@ -0,0 +1,10 @@
+{
+    "name" : "topaz_sales_custom_module",
+    "website" : "www.topazsmart.com",
+    "depends" : [
+        "sale"
+    ],
+    "data" : [
+        "views/sales_new_fields.xml"
+    ]
+}
