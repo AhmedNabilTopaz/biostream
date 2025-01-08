fcd719c6966a02f342ffd133cb68034fd73221ca
Mohamed Azaz <mohaz14@outlook.com>
2025-01-09 00:21:48 +0200

    add pre invoice

--- sales_topaz/__manifest__.py
+++ sales_topaz/__manifest__.py
@@ -1,10 +1,12 @@
 {
-    "name" : "topaz_sales_custom_module",
+    "name" : "Topaz custom module",
     "website" : "www.topazsmart.com",
     "depends" : [
-        "sale"
+        "sale",'stock'
     ],
     "data" : [
-        "views/sales_new_fields.xml"
+        "views/sales_new_fields.xml",
+        'report/report_deliveryslip.xml',
+        'report/stock_report_views.xml'
     ]
 }
